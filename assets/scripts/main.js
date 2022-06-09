bd965caf47e68f2d6dff2b5bb5a81999ce63f814
zerotume <zerotume@gmail.com>
2022-06-08 17:08:57 -0700

    things remaining

---
+++ assets/scripts/main.js
@@ -0,0 +1,14 @@
+/*
+
+    things remaining:
+    drag
+    put
+    checking the goal
+    blocking everything after the goal
+    the reward page XD
+    changing levels
+    storing progress
+
+
+
+*/
